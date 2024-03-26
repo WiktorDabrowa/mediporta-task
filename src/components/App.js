46ee8b272a2b7ad60ec6158c1b5a9d1deacf39e9
WiktorDabrowa <wiktor.dabrowa98@gmail.com>
2024-03-26 19:05:26 +0100

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mediporta task</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
